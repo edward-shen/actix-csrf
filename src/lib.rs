448021e4a60f834fe238471ea9b7c8b3ce60953e
siblingsofthevoid <e.vanemelen@proton.me>
2024-09-30 17:43:09 +0200

    Add documentation note for actix-multipart

--- src/lib.rs
+++ src/lib.rs
@@ -33,6 +33,10 @@
 //! secured and only accept HTTPS connections. Additionally, XSS attacks will
 //! render all CSRF mitigation techniques ineffective!
 //!
+//! ## Features:
+//! - `actix-multipart` Enables compatibility with
+//!     the [`actix_multipart`] library.
+//!
 //! ## Usage
 //!
 //! Using this middleware is simple. You just need to configure which endpoints
@@ -99,6 +103,42 @@
 //!
 //! For simple but complete examples, see the [examples] directory.
 //!
+//! ## Usage with [`actix_multipart`]
+//! <div class="warning">
+//!
+//! This requires the `actix-multipart` feature to be enabled
+//!
+//! </div>
+//!
+//! Usage with [`actix_multipart`] is almost the same as using it with
+//! actix's buiiltin [`Form`](actix_web::web::Form), with one exception:
+//! the `csrf_token` field must have type [`Text<CsrfToken>`](actix_multipart::form::text::Text)
+//! to interpret the CSRF token from the
+//! multipart form data. This works as follows:
+//!```
+//! # use actix_web::{HttpResponse, Responder, post};
+//! use actix_multipart::form::{text::Text, MultipartForm};
+//! use actix_csrf::extractor::{Csrf, CsrfGuarded, CsrfToken};
+//!
+//! #[derive(MultipartForm)]
+//! struct LoginForm { csrf_token: Text<CsrfToken> }
+//!
+//! impl CsrfGuarded for LoginForm {
+//!     fn csrf_token(&self) -> &CsrfToken {
+//!         &self.csrf_token
+//!     }
+//! }
+//!
+//! #[post("/login")]
+//! async fn login(form: Csrf<MultipartForm<LoginForm>>) -> impl Responder {
+//!     // The CSRF token is valid, so the request can proceed.
+//!
+//!     // ...Do other stuff here...
+//!
+//!     HttpResponse::Ok().finish()
+//! }
+//! ```
+
 //! ## Defense-in-depth Measures
 //!
 //! By default, this middleware multiple various defense-in-depth measures, such
